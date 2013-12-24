53096a82cb76b197739fafa1e2cbbb4dae50c67d
unknown <yaki.ams@gmail.com>
2013-12-24 09:01:56 +0200

    first commit

---
+++ src/com/example/WikiCity/MyActivity.java
@@ -0,0 +1,25 @@
+package com.example.WikiCity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import com.haarman.listviewanimations.itemmanipulation.ExpandableListItemAdapter;
+import com.haarman.listviewanimations.swinginadapters.prepared.SwingRightInAnimationAdapter;
+import com.haarman.listviewanimations.view.DynamicListView;
+
+public class MyActivity extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+
+    }
+}
