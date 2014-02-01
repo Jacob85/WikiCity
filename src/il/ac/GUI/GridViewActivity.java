74ad3152532fa76bf68fcd9eeade1617dfb4cea2
Jacob85 <yaki.ams@gmail.com>
2014-02-01 20:10:56 +0200

    fixed the Downloader Image Task

--- src/il/ac/GUI/GridViewActivity.java
+++ src/il/ac/GUI/GridViewActivity.java
@@ -6,7 +6,7 @@ import android.widget.GridView;
 import com.example.WikiCity.R;
 import com.haarman.listviewanimations.swinginadapters.prepared.SwingBottomInAnimationAdapter;
 import il.ac.adapters.GridViewImagesAdapter;
-import il.ac.shenkar.common.cityEnumType;
+import il.ac.shenkar.common.cityEnumType ;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,10 +30,10 @@ public class GridViewActivity extends Activity
 
     }
 
-    public ArrayList<cityEnumType> getAllItems()
+    public ArrayList<cityEnumType > getAllItems()
     {
-        ArrayList<cityEnumType> toReturn;
-        toReturn = new ArrayList<cityEnumType>(Arrays.asList(cityEnumType.values()));
+        ArrayList<cityEnumType > toReturn;
+        toReturn = new ArrayList<cityEnumType >(Arrays.asList(cityEnumType .values()));
         return toReturn;
 
     }
