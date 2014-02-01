d111cbb0118302cf4c78b14773a8a63c0adbd53c
Jacob85 <yaki.ams@gmail.com>
2014-02-02 01:42:58 +0200

    for Hanny

--- src/il/ac/GUI/WebViewFragment.java
+++ src/il/ac/GUI/WebViewFragment.java
@@ -10,6 +10,8 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import il.ac.shenker.wiki.PageSection;
 
+import java.net.URL;
+
 /**
  * Created by Amsalem on 01/02/14.
  */
@@ -18,6 +20,8 @@ public class WebViewFragment extends DialogFragment
     Context context;
     PageSection pageSection;
     WebView webView;
+    URL url;
+    boolean isUrlOnly = false;
 
     public WebViewFragment(Context context, PageSection pageSection)
     {
@@ -25,34 +29,72 @@ public class WebViewFragment extends DialogFragment
         this.pageSection = pageSection;
     }
 
+    public WebViewFragment(Context context, URL url)
+    {
+        this.context = context;
+        this.url = url;
+        isUrlOnly = true;
+
+    }
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         super.onCreateDialog(savedInstanceState);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle(pageSection.getSectionName());
+        if(!isUrlOnly)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle(pageSection.getSectionName());
 
-        webView = new WebView(context);
-        //webView.loadData(pageSection.getSecrionContentInHtml(), "text/html", "UTF-8");
-        webView.loadDataWithBaseURL("http://en.wikipedia.org/", pageSection.getSecrionContentInHtml(), "text/html", "UTF-8", null);
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+            webView = new WebView(context);
+            //webView.loadData(pageSection.getSecrionContentInHtml(), "text/html", "UTF-8");
+            webView.loadDataWithBaseURL("http://en.wikipedia.org/", pageSection.getSecrionContentInHtml(), "text/html", "UTF-8", null);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
 
-                return true;
-            }
-        });
+                    return true;
+                }
+            });
 
-        builder.setView(webView);
-        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.dismiss();
-            }
-        });
+            builder.setView(webView);
+            builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
 
-        return builder.create();
+            return builder.create();
+        }
+        else
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle(url.toString());
+            webView = new WebView(context);
+            webView.loadUrl(url.toString());
+
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+
+                    return true;
+                }
+            });
+
+            builder.setView(webView);
+            builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+
+            return builder.create();
+        }
     }
 }
