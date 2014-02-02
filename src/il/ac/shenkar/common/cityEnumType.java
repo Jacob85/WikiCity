a3d9e685d824121d790cd3870a90261759735805
Jacob85 <yaki.ams@gmail.com>
2014-02-02 03:04:54 +0200

    Added the fonts

--- src/il/ac/shenkar/common/cityEnumType.java
+++ src/il/ac/shenkar/common/cityEnumType.java
@@ -9,24 +9,24 @@ import java.io.Serializable;
  */
 public enum cityEnumType  implements Serializable
 {
-    LOS_ANGELES("Los Angeles", R.drawable.losangeles,"California", 38040000, "fonts/Yes Please - DeliciousPro.oft"),
-  //  LAS_VEGAS("Las Vegas", R.drawable.losangeles,"Nevada"),
-    MIAMI("Miami", R.drawable.miami, "Florida", 19552860, "fonts/futura LT condensd.ttf"),
-    NEY_YORK("New York City", R.drawable.newyork3, "New York" , 19651127, "fonts/ENGLISHT.TTF" ),
-    CHICAGO ("Chicago", R.drawable.chcago, "Illinois", 12882135, "fonts/Rex Bold Inline.oft"),
-    BALTIMORE("Baltimore", R.drawable.baltimore, "Maryland", 5928814, "fonts/American Type writer LT cn.ttf" ),
-    BOSTON("Boston", R.drawable.boston, "Massachusetts", 6692824, "fonts/futura LT condensd.ttf" ),
-    DALLAS("Dallas ", R.drawable.dallas, "Texas", 26448193, "fonts/Weston Free.otf"  ),
-    DENVER("Denver_City,_Texas", R.drawable.denver, "Texas", 26448193, "fonts/Weston Free.otf"),
-    HONOLULU("Honolulu", R.drawable.honololo, "Hawaii", 1404054, "fonts/futura LT condensd.ttf" ),
-    HOUSTON("Houston", R.drawable.houston, "Texas", 26448193, "fonts/Weston Free.otf" ),
-    ORLANDO("Orlando,_Florida", R.drawable.orlando, "Florida", 19552860, "fonts/futura LT condensd.ttf"),
-    PHOENIX("Phoenix", R.drawable.phonix, "Arizona", 6626624, "fonts/BOOKOSB.TTF" ),
-    SAN_FRANCISCO("San Francisco", R.drawable.sanfransico7, "California", 38040000,"fonts/Yes Please - DeliciousPro.oft" ),
-    SEATTLE("Seattle", R.drawable.seattle1, "Washington", 6971406, "fonts/Kankin.otf" ),
-    TULSA("Tulsa,_Oklahoma", R.drawable.tulsa, "Oklahoma", 3850568, "fonts/Kankin.otf" ),
-    WASHINGTON("Washington,_D.C.", R.drawable.washington, "Washington", 6971406, "fonts/Kankin.otf"),
-    OKLAHOMA("Oklahoma_City", R.drawable.oklahoma, "Oklahoma", 3850568,"fonts/Kankin.otf" );
+    LOS_ANGELES("Los Angeles", R.drawable.losangeles,"California", 38040000, "fonts/Yes Please - DeliciousPro.oft", R.drawable.losangelsbk),
+    LAS_VEGAS("Las Vegas", R.drawable.lasvegas,"Nevada" , 2758931, "fonts/LTE50181.TTF" , R.drawable.lasvegasbk),
+    MIAMI("Miami", R.drawable.miami, "Florida", 19552860, "fonts/futura LT condensd.ttf", R.drawable.miami),
+    NEY_YORK("New York City", R.drawable.newyork3, "New York" , 19651127, "fonts/ENGLISHT.TTF", R.drawable.newyorkbk ),
+   // CHICAGO ("Chicago", R.drawable.chcago, "Illinois", 12882135, "fonts/Rex Bold Inline.oft"),
+    BALTIMORE("Baltimore", R.drawable.baltimore, "Maryland", 5928814, "fonts/American Type writer LT cn.ttf" , R.drawable.baltimorebackground),
+    BOSTON("Boston", R.drawable.boston, "Massachusetts", 6692824, "fonts/futura LT condensd.ttf", R.drawable.bostonbk ),
+    DALLAS("Dallas ", R.drawable.dallas, "Texas", 26448193, "fonts/Weston Free.otf" , R.drawable.dallasbk ),
+    DENVER("Denver_City,_Texas", R.drawable.denver, "Texas", 26448193, "fonts/Weston Free.otf", R.drawable.denverbk),
+    HONOLULU("Honolulu", R.drawable.honololo, "Hawaii", 1404054, "fonts/futura LT condensd.ttf" , R.drawable.honololobk),
+    HOUSTON("Houston", R.drawable.houston, "Texas", 26448193, "fonts/Weston Free.otf", R.drawable.houstonbk),
+    ORLANDO("Orlando,_Florida", R.drawable.orlando, "Florida", 19552860, "fonts/futura LT condensd.ttf", R.drawable.orlandobk),
+    PHOENIX("Phoenix", R.drawable.phonix, "Arizona", 6626624, "fonts/BOOKOSB.TTF", R.drawable.phonix ),
+    SAN_FRANCISCO("San Francisco", R.drawable.sanfransico7, "California", 38040000,"fonts/Yes Please - DeliciousPro.oft", R.drawable.sanfransicobk),
+    SEATTLE("Seattle", R.drawable.seattle1, "Washington", 6971406, "fonts/Kankin.otf" , R.drawable.seattlebk),
+    TULSA("Tulsa,_Oklahoma", R.drawable.tulsa, "Oklahoma", 3850568, "fonts/Kankin.otf" , R.drawable.tulsabk),
+    WASHINGTON("Washington,_D.C.", R.drawable.washington, "Washington", 6971406, "fonts/Kankin.otf", R.drawable.washingtonbk),
+    OKLAHOMA("Oklahoma_City", R.drawable.oklahoma, "Oklahoma", 3850568,"fonts/Kankin.otf" , R.drawable.oklahomabk);
 
 
 
@@ -35,13 +35,19 @@ public enum cityEnumType  implements Serializable
     private String stateName;
     private int totalStatePopulation;
     private String pathToFont;
+    private int imageBackGroundId;
 
-    cityEnumType (String cityName, int imageId, String stateName, int totalStatePopulation, String pathToFont) {
+    cityEnumType (String cityName, int imageId, String stateName, int totalStatePopulation, String pathToFont, int imageBackGroundId) {
         this.cityName = cityName;
         this.imageId = imageId;
         this.stateName = stateName;
         this.totalStatePopulation = totalStatePopulation;
         this.pathToFont = pathToFont;
+        this.imageBackGroundId = imageBackGroundId;
+    }
+
+    public int getImageBackGroundId() {
+        return imageBackGroundId;
     }
 
     public int getTotalStatePopulation() {
